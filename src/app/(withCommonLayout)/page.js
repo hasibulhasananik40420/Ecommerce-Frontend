1a1cc7434cb1ef781b0dc03eccaeddceafadd1c0
Omar Faruk <150340823+OmarDevZon@users.noreply.github.com>
2024-06-03 19:25:11 +0600

    new products

--- src/app/(withCommonLayout)/page.js
+++ src/app/(withCommonLayout)/page.js
@@ -1,20 +1,25 @@
-import HomePageSlider from '../../components/ui/Silder/HomePageSlider'
-import DiscountLaptop from '../../components/ui/DiscountLaptop/DiscountLaptop'
-import Policy from '../../components/ui/Policy/Policy'
-import FlashSale from '../../components/ui/FlashSale/FlashSale'
-import AllCategories from '../../components/ui/AllCategories/AllCategories'
+import HomePageSlider from "../../components/ui/Silder/HomePageSlider";
+import DiscountLaptop from "../../components/ui/DiscountLaptop/DiscountLaptop";
+import Policy from "../../components/ui/Policy/Policy";
+import FlashSale from "../../components/ui/FlashSale/FlashSale";
+import AllCategories from "../../components/ui/AllCategories/AllCategories";
+import TopProducts from "../../components/ui/home/topProducts/TopProducts";
+import NewProducts from "../../components/ui/home/newProducts/newProducts";
+import SliderCard from "../../components/ui/Silder/SliderCard";
 
 const HomePage = () => {
   return (
     <div>
-       <AllCategories/>
-       <HomePageSlider/>
-        <Policy/>
-      
-       <DiscountLaptop/>
-       <FlashSale/>
-    </div>
-  )
-}
+      {/* <AllCategories />
+      <HomePageSlider />
+      <Policy />
 
-export default HomePage
+      <DiscountLaptop />
+      <FlashSale /> */}
+      <NewProducts />
+      <TopProducts />
+    </div>
+  );
+};
+
+export default HomePage;
--- src/components/ui/Silder/SliderCard.js
+++ src/components/ui/Silder/SliderCard.js
@@ -11,7 +11,7 @@ import Image from "next/image"
 
 const SliderCard = () => {
     const settings = {
-        dots: true,
+    dots: true,
     infinite: true,
     autoplay: true,
     speed: 500,
